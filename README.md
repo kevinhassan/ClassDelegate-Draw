b3cd517ab53dab4d77d8d247a37345eb9ff4b757
Kévin Hassan <kevin.hassan@etu.umontpellier.fr>
2016-09-24 22:14:36 +0200

    add: final project

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# electionRandomAccess
-Get a number of name from a xlsx file
+# TirageAuSort
+
+## Installation :
+
+You need to install xlrd module to extract data from Microsoft Excel spreadsheet
+Download it here : [**link**](https://pypi.python.org/pypi/xlrd#downloads).
+
+** Launch installation with : **
+
+```
+  $ tar xz xlrd-x.x.x.tar.gz
+```
+```  
+  $ cd xlrd-x.x.x
+```   
+```
+  $ python easy-install.py
+```
+
+## Launch program
+
+```
+  $ python tirageAuSort.py -f [nameFile.xlsx] -n [number]
+```
---
+++ tirageAuSort.py
@@ -0,0 +1,65 @@
+#This function extract the list of students after xls opening
+def extractStudents(filename):
+    """
+        Pre: The list in xls file is not empty
+        Post: All students are extract from file
+        Returns students list
+    """
+    list = []
+    try:
+        # open Excel file
+        wb = xlrd.open_workbook(str(filename))
+    except IOError:
+        print "Oops! No file "+filename+ " has been found !"
+    else:
+        sh = wb.sheet_by_name(wb.sheet_names()[0])
+        for rownum in range(1,sh.nrows):#1 to remove title line
+            student = sh.row_values(rownum)
+            list.append(student)
+    return list
+
+import sys,getopt,random,xlrd
+
+def main(argv):
+   filename = ''
+   n = -1
+   student = []
+   try:
+      options, remainder = getopt.getopt(sys.argv[1:], 'f:n:', ['--file=','--number='])
+   except getopt.GetoptError:
+      print sys.argv[0] + ' -f <filename> -n <numberOfName>'
+      sys.exit(2)
+   else:
+       for opt, arg in options:
+        if opt == '-h':
+            print sys.argv[0] + '-f <filename> -n <numberOfName>'
+            sys.exit()
+        elif opt in ("-f", "--file"):
+            filename = str(arg)
+        elif opt in ("-n", "--number"):
+            n = int(arg)
+   if filename!='' and n!=-1:
+       students = extractStudents(filename)
+       if (len(students)<n):
+           print('No need to launch program because you have only '+str(n)+' students')
+           sys.exit()
+       else:
+           numbers = range(len(students))#generate table [0, n-1]
+           i = len(numbers)-n
+           while i < len(numbers): #Remove n index from numbers and students
+            numbers = range(len(students))#generate table [0, n-1]
+            k = len(numbers)-1
+            l = random.randint(0,k)
+            j = numbers[l] #Get int in the list of number
+            student.append(students[j])#add student selected
+            del students[j]#Remove this student from the list
+            del numbers[l]#Remove the number from list to avoid int random repetition
+           print "Les candidats pour les elections sont : "
+           for candidat in student:
+               print candidat[0], candidat[1]
+   else:
+       print('error occured')
+       sys.exit()
+
+if __name__ == "__main__":
+   main(sys.argv[1:])
